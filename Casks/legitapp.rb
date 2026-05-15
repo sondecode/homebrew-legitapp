b3f7a59cafcb9500cec14be94a74638e28ffe42c
Son decode <62468865+sondecode@users.noreply.github.com>
2026-05-16 01:17:08 +0700

    Downgrade version from 1.0.2 to 1.0.0

--- Casks/legitapp.rb
+++ Casks/legitapp.rb
@@ -1,5 +1,5 @@
 cask "legitapp" do
-  version "1.0.2"
+  version "1.0.0"
   sha256 "24c5f9dd8a758e84df6cea61a8be6af918102af2a607651a7c8751d1ef7ab1e9"
 
   url "https://github.com/sondecode/legitapp/releases/download/v#{version}/LegitApp.dmg"
